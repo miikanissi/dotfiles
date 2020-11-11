1c6dd26cfe4bcf109d2a3c698628f8dccd758a4b
Miika <miika@miikanissi.com>
2020-11-11 22:20:13 +0200

    check if dir is mountpoint

--- .local/bin/backup.sh
+++ .local/bin/backup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+mount_point=/media/miika/backup
 
-rsync -avxP --delete-after --exclude '.local/share/Trash' --exclude '.cache' ~/ /media/miika/backup/
+if mountpoint -q -- $mount_point; then
+  rsync -avxPW --delete-after --exclude '.local/share/Trash' --exclude '.cache' ~/ $mount_point
+fi
