86cabfc6f3c6f70337745fc021e845a1d4afaa48
Miika <miika@miikanissi.com>
2020-11-19 16:31:16 +0200

    very minor changes

--- .local/bin/audio_loop.sh
+++ .local/bin/audio_loop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # uses pacat for audio loopback
+killall -q pacat
 
 pacat -r --latency-msec=1 -d alsa_input.pci-0000_0c_00.4.analog-stereo | pacat -p --latency-msec=1 -d alsa_output.usb-FiiO_DigiHug_USB_Audio-01.analog-stereo &
--- .local/bin/transadd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Mimeapp script for adding torrent to transmission-daemon, but will also start the daemon first if not running.
-
-# transmission-daemon sometimes fails to take remote requests in its first moments, hence the sleep.
-
-transmission-gtk "$@" && notify-send "Torrent added."
