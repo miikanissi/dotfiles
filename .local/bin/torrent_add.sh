5e6b32e109cd375ad031addcd8f6d3bc56c7d7ef
Miika <miika@miikanissi.com>
2020-11-22 22:54:58 +0200

    fixed torrent desktop file

--- .local/bin/torrent_add.sh
+++ .local/bin/torrent_add.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+
 # Mime map script for adding a magnet link to transmission, and starts the daemon if it's not running
 # Sends a notification when a torrent is added to transmission
 
 pgrep -x transmission-daemon > /dev/null || transmission-daemon
 
-transmission-remote --add "$@" && notify-send "Transmission-daemon" "Torrent added." && polybar-msg hook torrents 1
+transmission-remote -a "$@" && notify-send "Transmission-daemon" "Torrent added."
