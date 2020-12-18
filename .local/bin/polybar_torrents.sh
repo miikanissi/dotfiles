7782f1ef5b290b8e14e218f5774091d9419f0886
Miika <miika@miikanissi.com>
2020-12-18 22:27:27 +0200

    polybar torrent script changed

--- .local/bin/polybar_torrents.sh
+++ .local/bin/polybar_torrents.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 # display number of torrents downloading, paused and seeding
 
-torrents=$(transmission-remote -l)
-downloading=$(echo "$torrents" | grep "Downloading\|Up & Down" | wc -l)
-paused=$(echo "$torrents" | grep "Stopped" | wc -l)
-seeding=$(echo "$torrents" | grep "Seeding" | wc -l)
-idle=$(echo "$torrents" | grep "Idle" | wc -l)
+if [ -z $(pidof transmission-daemon) ]; then
+  echo "%{T2}%{F#c85e7c}%{F-}%{T-} 0 %{T2}%{F#c85e7c}%{F-}%{T-} 0 %{T2}%{F#c85e7c}%{F-}%{T-} 0 %{T2}%{F#c85e7c}%{F-}%{T-} 0"
+else
+  torrents=$(transmission-remote -l)
+  downloading=$(echo "$torrents" | grep "Downloading\|Up & Down" | wc -l)
+  paused=$(echo "$torrents" | grep "Stopped" | wc -l)
+  seeding=$(echo "$torrents" | grep "Seeding" | wc -l)
+  idle=$(echo "$torrents" | grep "Idle" | wc -l)
 
-echo "%{T2}%{F#c85e7c}%{F-}%{T-} $downloading %{T2}%{F#c85e7c}%{F-}%{T-} $seeding %{T2}%{F#c85e7c}%{F-}%{T-} $paused %{T2}%{F#c85e7c}%{F-}%{T-} $idle"
+  echo "%{T2}%{F#c85e7c}%{F-}%{T-} $downloading %{T2}%{F#c85e7c}%{F-}%{T-} $seeding %{T2}%{F#c85e7c}%{F-}%{T-} $paused %{T2}%{F#c85e7c}%{F-}%{T-} $idle"
+fi
