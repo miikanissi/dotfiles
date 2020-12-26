31d6250d4bbc5208d5e3b954338e99c9cbf26296
Miika <miika@miikanissi.com>
2020-12-26 19:55:30 +0200

    cleaned deprecated configs

--- .local/bin/keyboard.sh
+++ .local/bin/keyboard.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 xset r rate 300 50 & # makes key repeat faster
-setxkbmap -option grp:switch -option grp_led:scroll us,fi
+setxkbmap -option 'ctrl:swapcaps' -option grp:switch -option grp_led:scroll us,fi
