63c1b11a49ea813d3d236cf75c150a715f30a0ef
Miika <miika@miikanissi.com>
2021-01-02 14:49:58 +0200

    added rofi script to kill process

---
+++ .local/bin/rofi_killprocess.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# by Miika Nissi, https://miikanissi.com, https://github.com/miikanissi
+# pipes list of processes into rofi/dmenu and kills the selection
+
+proc=$(ps -u $USER -o pid,%mem,%cpu,comm | sort -b -k2 -r | sed -n '1!p' | rofi -dmenu -i | awk '{print $1,$4}')
+
+[ -z "$proc" ] || (kill -15 $(echo $proc | awk '{print $1}') 2>/dev/null && notify-send "$(echo $proc | awk '{print $2}') killed")
