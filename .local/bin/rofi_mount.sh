2cf832b5dd58b1fc4197b01f0acabfb6454c6a39
Miika <miika@miikanissi.com>
2021-01-25 14:28:12 +0200

    rofi mount script v0.1

--- .local/bin/rofi_mount.sh
+++ .local/bin/rofi_mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # by Miika Nissi, https://miikanissi.com, https://github.com/miikanissi
 
-prompt="$(echo -e "mount\numount\nlsblk" | rofi -dmenu)"
+prompt="$(echo -e "mount\numount\neject\nlsblk" | rofi -dmenu)"
 if [ $prompt = mount ]; then
   dev=$(echo $(lsblk -lp | grep -E "/dev/.*part" | awk '{print $1}' | rofi -dmenu))
   [ -z $dev ] && exit 0;
@@ -10,17 +10,14 @@ if [ $prompt = mount ]; then
     if $(sudo cryptsetup isLuks $dev); then
       pass=$(rofi -dmenu -password -p "Enter passphrase for $dev:")
       dir=$(find /mnt/ -mindepth 1 -maxdepth 1 | rofi -dmenu -p "Mount $dev to:")
-      [[ -z $pass || -z $dir ]] || exit 0;
+      [[ -z $pass || -z $dir ]] && exit 0;
+      echo hi
       echo -n $pass | sudo cryptsetup luksOpen $dev $devname -d -
-      if [ -c /dev/mapper/$devname ]; then
-        sudo mount /dev/mapper/$devname $dir
-        if mountpoint -q -- $dir; then
-          notify-send "$dev mounted to $dir"
-        else
-          notify-send "Mount failed"
-        fi
+      sudo mount /dev/mapper/$devname $dir
+      if mountpoint -q -- $dir; then
+        notify-send "$dev mounted to $dir"
       else
-        notify-send "luksOpen failed"
+        notify-send "Mount failed"
       fi
     else
       dir=$(find /mnt/ -mindepth 1 -maxdepth 1 | rofi -dmenu -p "Mount $dev to:")
@@ -35,6 +32,34 @@ if [ $prompt = mount ]; then
   else
     notify-send "$dev is already mounted"
     exit 0;
+  fi
+elif [ $prompt = umount ]; then
+  dev=$(echo $(lsblk -lp | grep -E "/dev/.*part|/dev/.*crypt" | awk '{print $1}' | rofi -dmenu))
+  [ -z $dev ] && exit 0;
+  if [ -z $(mount | grep $dev) ]; then
+    notify-send "$dev is already unmounted"
+    exit 0;
+  else
+    sudo umount $dev
+    if [ -z $(mount | grep $dev) ]; then
+      if [ -z $(echo $dev | grep "mapper") ]; then
+        notify-send "$dev unmounted succesfully"
+      else
+        sudo cryptsetup luksClose $dev
+        notify-send "$dev unmounted succesfully"
+      fi
+    else
+      notify-send "Unmount failed"
+    fi
+  fi
+elif [ $prompt = eject ]; then
+  dev=$(echo $(lsblk -lp | grep -E "/dev/.*disk" | awk '{print $1}' | rofi -dmenu))
+  [ -z $dev ] && exit 0;
+  sudo eject $dev
+  if [ -z $(lsblk -lp | grep $dev) ]; then
+    notify-send "$dev ejected succesfully"
+  else
+    notify-send "Eject failed"
   fi
 elif [ $prompt = lsblk ]; then
   notify-send "$(lsblk -o NAME,SIZE,TYPE,MOUNTPOINT)"
