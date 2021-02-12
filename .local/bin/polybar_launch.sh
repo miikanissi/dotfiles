19c9bb6601c2b805c6c5eebad2662a6c53377d42
Miika Nissi <miika@miikanissi.com>
2021-02-12 19:50:59 +0200

    redesigned polybar

--- .local/bin/polybar_launch.sh
+++ .local/bin/polybar_launch.sh
@@ -4,12 +4,13 @@ killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 0.5; done
 
 outputs=$(xrandr --listactivemonitors|awk '{print $4}'|sed '/^$/d')
+primary_monitor=$(xrandr | grep primary | cut -d ' ' -f 1)
 
 for m in $outputs; do
   export MONITOR=$m
   if [[ "$(hostname)" == "TA-NISMI-E490" ]]; then
     MONITOR=$m polybar --reload bar-e490 -c ~/.config/polybar/config &
   else
-    MONITOR=$m polybar --reload bar-t440p -c ~/.config/polybar/config &
+    MONITOR=$m polybar main -c ~/.config/polybar/t440p-minimal.ini &
   fi
 done
