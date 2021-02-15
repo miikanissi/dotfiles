d6677bca43b7c37c04129edf5bb6c14a42e745a5
Miika Nissi <miika@miikanissi.com>
2021-02-15 21:11:26 +0200

    [IMP] Improved polybar launching

--- .local/bin/polybar_launch.sh
+++ .local/bin/polybar_launch.sh
@@ -1,16 +1,47 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# by Miika Nissi, https://miikanissi.com, https://github.com/miikanissi
+# launches polybar
+(
+  flock 200
+  killall -q polybar
+  while pgrep -u $UID -x polybar > /dev/null; do sleep 2; done
 
-killall -q polybar
-while pgrep -u $UID -x polybar > /dev/null; do sleep 2; done
+  PRIMARY_MONITOR=$(xrandr | grep primary | cut -d ' ' -f 1)
+  MONITORS=$(xrandr --listactivemonitors|awk '{print $4}'|sed '/^$/d')
+  HOSTNAME=$(hostname)
+  TRAY=eDP-1
 
-outputs=$(xrandr --listactivemonitors|awk '{print $4}'|sed '/^$/d')
-hostname=$(hostname)
-
-for m in $outputs; do
-  export MONITOR=$m
-  if [[ "$hostname" == "TA-NISMI-E490" ]]; then
-    MONITOR=$m polybar e490 -c ~/.config/polybar/minimal.ini &
-  else
-    MONITOR=$m polybar t440p -c ~/.config/polybar/minimal.ini &
+  if [[ "$HOSTNAME" == "stinkpad" ]]; then
+    for m in $MONITORS; do
+      if [[ $m == $PRIMARY_MONITOR ]]; then
+        TRAY=$m
+      fi
+    done
+    for m in $MONITORS; do
+      if [[ $m == $PRIMARY_MONITOR ]]; then
+        MONITOR1=$m polybar --reload main -c ~/.config/polybar/t440p_minimal.ini </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+        export TRAY_POSITION=right
+      else
+        MONITOR2=$m polybar --reload secondary -c ~/.config/polybar/t440p_minimal.ini </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+        export TRAY_POSITION=none
+      fi
+      disown
+    done
+  elif [[ "$HOSTNAME" == "TA-NISMI-E490" ]]; then
+    for m in $MONITORS; do
+      if [[ $m == $PRIMARY_MONITOR ]]; then
+        TRAY=$m
+      fi
+    done
+    for m in $MONITORS; do
+      if [[ $m == $PRIMARY_MONITOR ]]; then
+        MONITOR1=$m polybar --reload main -c ~/.config/polybar/e490_minimal.ini </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+        export TRAY_POSITION=right
+      else
+        MONITOR2=$m polybar --reload secondary -c ~/.config/polybar/e490_minimal.ini </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+        export TRAY_POSITION=none
+      fi
+      disown
+    done
   fi
-done
+) 200>/var/tmp/polybar-launch.lock
