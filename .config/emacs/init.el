feaa774e93c75b42649c4db031fdbed4eeceb060
Miika Nissi <miika@miikanissi.com>
2021-02-26 23:28:03 +0200

    some minor Emacs improvement and cleanup

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -10,23 +10,3 @@
 
 ;; config organized in org-mode
 (org-babel-load-file (expand-file-name "~/.config/emacs/config.org"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(gruvbox-dark-hard))
- '(custom-safe-themes
-   '("6b5c518d1c250a8ce17463b7e435e9e20faa84f3f7defba8b579d4f5925f60c1" default))
- '(line-number-mode nil)
- '(package-selected-packages
-   '(eterm-256color symon fancy-battery diminish rg swiper auto-package-update mingus simple-mpc emms magit company-shell company-c-headers flycheck-clang-analyzer flycheck yasnippet-snippets yasnippet prettier-js web-mode company-jedi company-irony ox-twbs htmlize popup-kill-ring linum-relative ido-completing-read+ async doom-modeline spaceline company dashboard rainbow-delimiters sudo-edit switch-window rainbow-mode avy smex ido-vertical-mode beacon gruvbox-theme which-key use-package))
- '(size-indication-mode t)
- '(symon-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "IBM " :family "BlexMono Nerd Font")))))
