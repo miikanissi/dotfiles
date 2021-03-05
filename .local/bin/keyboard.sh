e4d09cebc8a03cc0568a7cae1408e873623e32d3
Miika Nissi <miika@miikanissi.com>
2021-03-05 23:12:02 +0200

    Initial Emacs blog site

--- .local/bin/keyboard.sh
+++ .local/bin/keyboard.sh
@@ -2,4 +2,4 @@
 
 xset m 1 1 & # mouse acceleration off
 xset r rate 200 70 & # makes key repeat faster
-setxkbmap -option 'ctrl:swapcaps' -option grp:switch -option grp_led:scroll us,fi
+setxkbmap -option 'ctrl:nocaps' -option grp:switch -option grp_led:scroll us,fi
