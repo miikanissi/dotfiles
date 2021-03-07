24c734d9f0850f278208e1f9ac0746c57122d573
Miika Nissi <miika@miikanissi.com>
2021-03-07 15:12:21 +0200

    Emacs config for a blog

--- .local/bin/rsync_website.sh
+++ .local/bin/rsync_website.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -uvrP -e 'ssh -p 69' --delete-after ~/Code/web/miikanissi.com/ root@miikanissi.com:/var/www/
+rsync -uvrP -e 'ssh -p 69' --delete-after ~/miikanissi.com/www/ root@miikanissi.com:/var/www/www/
