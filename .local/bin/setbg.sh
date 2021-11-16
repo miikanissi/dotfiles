a283a15b634565f5ec05555c879ebf8f244b2d5a
Miika Nissi <miika@miikanissi.com>
2021-11-16 17:59:23 -0500

    Wallpaper setup

--- .local/bin/setbg.sh
+++ .local/bin/setbg.sh
@@ -1,6 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+
+get_active_monitors()
+{
+    xrandr | awk '/\ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $1}'
+}
+COUNT=$(get_active_monitors | wc -l)
 
 [ -f "$1" ] && cp "$1" ~/.local/share/wall.jpg
 [ -f "$2" ] && cp "$2" ~/.local/share/wall-vertical.jpg
-feh --no-fehbg --bg-scale ~/.local/share/wall.jpg --no-fehbg --bg-scale ~/.local/share/wall-vertical.jpg
-# xwallpaper --zoom ~/.config/gallery/wall.jpg
+
+if [ "$COUNT" -eq 2 ]; then
+  feh --no-fehbg --bg-scale ~/.local/share/wall.jpg --bg-scale ~/.local/share/wall-vertical.jpg
+elif [ "$COUNT" -gt 2 ]; then
+  feh --no-fehbg --bg-scale ~/.local/share/wall.jpg --no-fehbg --bg-scale ~/.local/share/wall.jpg --no-fehbg --bg-scale ~/.local/share/wall-vertical.jpg
+else
+  feh --no-fehbg --bg-scale ~/.local/share/wall.jpg
+fi
