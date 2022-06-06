5c8eca8f555d3fb08efe2752c3f99640252d06c6
Miika Nissi <miika@miikanissi.com>
2022-06-05 21:53:20 -0400

    Working version of monitor hotplug setup. Remove autorandr as it does not work correctly

--- .local/bin/bspwm_setup_monitors.sh
+++ .local/bin/bspwm_setup_monitors.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-## Notify BSPWM to run this script for all xrandr changes by adding it to:
-## ~/.config/autorandr/postswitch
-## See Hook scripts: https://github.com/phillipberndt/autorandr
 
 # Exit script if not on bspwm
 WM=$(wmctrl -m | grep Name: | awk '{print $2}')
@@ -9,8 +6,8 @@ if [[ "${WM}" != "bspwm" ]]; then
   exit 0
 fi
 
-INTERNAL_MONITOR=eDP
-EXTERNAL_MONITOR=HDMI1
+INTERNAL_MONITOR="eDP"
+EXTERNAL_MONITOR="HDMI-A-0"
 
 monitor_add() {
   # Move first 5 desktops to external monitor
@@ -19,18 +16,11 @@ monitor_add() {
   done
   # Remove default desktop created by bspwm
   bspc desktop Desktop --remove
+  # reorder monitors
+  bspc wm -O "$EXTERNAL_MONITOR" "$INTERNAL_MONITOR"
 }
 
 monitor_remove() {
-  # Add default temp desktop because a minimum of one desktop is required per monitor
-  bspc monitor "$INTERNAL_MONITOR" -a Desktop
-
-  # Move all desktops except the last default desktop to disconnected
-  # external monitor to reorder desktops
-  for desktop in $(bspc query -D -m "$INTERNAL_MONITOR"); do
-		bspc desktop "$desktop" --to-monitor "$EXTERNAL_MONITOR"
-	done
-
   # Add default temp desktop because a minimum of one desktop is required per monitor
   bspc monitor "$EXTERNAL_MONITOR" -a Desktop
 
@@ -41,24 +31,35 @@ monitor_remove() {
 
   # delete default desktops
   bspc desktop Desktop --remove
-  bspc desktop Desktop --remove
+  # reorder desktops
+  bspc monitor "$INTERNAL_MONITOR" -o 1 2 3 4 5 6 7 8 9 10
+
 }
 
-if [[ $(xrandr -q | grep -q "${EXTERNAL_MONITOR} connected") ]]; then
+if [[ $(xrandr -q | grep "${EXTERNAL_MONITOR} connected") ]]; then
+  # set xrandr rules for docked setup
+  xrandr --output "$INTERNAL_MONITOR" --mode 1920x1080 --pos 0x0 --rotate normal --output "$EXTERNAL_MONITOR" --primary --mode 1920x1080 --pos 1920x0 --rotate normal
+  if [[ $(bspc query -D -m "${EXTERNAL_MONITOR}" | wc -l) -ne 5 ]]; then
     monitor_add
+  fi
+  bspc wm -O "$EXTERNAL_MONITOR" "$INTERNAL_MONITOR"
 else
+  # set xrandr rules for mobile setup
+  xrandr --output "$INTERNAL_MONITOR" --primary --mode 1920x1080 --pos 0x0 --rotate normal --output "$EXTERNAL_MONITOR" --off
+  if [[ $(bspc query -D -m "${INTERNAL_MONITOR}" | wc -l) -ne 10 ]]; then
     monitor_remove
+  fi
 fi
 
 # Set wallpaper
 ~/.local/bin/setbg.sh &
 
-# Launch polybar
+# Kill and relaunch polybar
 killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 2; done
-
-polybar --reload primary -c ~/.config/polybar/config.ini </dev/null >/var/tmp/polybar-primary.log 2>&1 200>&- &
-
-if [[ $(xrandr -q | grep -q "${EXTERNAL_MONITOR} connected") ]]; then
+if [[ $(xrandr -q | grep "${EXTERNAL_MONITOR} connected") ]]; then
+  polybar --reload primary -c ~/.config/polybar/config.ini </dev/null >/var/tmp/polybar-primary.log 2>&1 200>&- &
   polybar --reload secondary -c ~/.config/polybar/config.ini </dev/null >/var/tmp/polybar-secondary.log 2>&1 200>&- &
+else
+  polybar --reload primary -c ~/.config/polybar/config.ini </dev/null >/var/tmp/polybar-primary.log 2>&1 200>&- &
 fi
