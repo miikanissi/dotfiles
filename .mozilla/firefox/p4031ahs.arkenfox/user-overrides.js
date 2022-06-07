98b7b6ad2e928d77184a74820e8833d8e3ce653a
Miika Nissi <miika@miikanissi.com>
2022-06-06 22:59:34 -0400

    Init firefox config

---
+++ .mozilla/firefox/p4031ahs.arkenfox/user-overrides.js
@@ -0,0 +1,25 @@
+/*********************
+ * arkenfox user.js overrides
+ *
+ * ******************/
+user_pref("browser.startup.page", 3); // 0102
+user_pref("privacy.clearOnShutdown.history", false); // 2811
+user_pref("keyword.enabled", true); // 0801
+user_pref("network.http.referer.XOriginPolicy", 2); // 1601
+user_pref("dom.disable_beforeunload", false); // 2401
+user_pref("browser.download.manager.addToRecentDocs", true); // 2653
+user_pref("network.cookie.lifetimePolicy", 0); //2801
+user_pref("privacy.clearOnShutdown.history", false); // 2811
+user_pref("privacy.clearOnShutdown.sessions", false);  // 2811
+user_pref("privacy.resistFingerprinting", false); //4504
+user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504
+user_pref("browser.display.use_system_colors", true); // 4510
+user_pref("webgl.disabled", false); // 4520
+user_pref("browser.warnOnQuitShortcut", true); // 9000
+user_pref("general.autoScroll", true); // 9000
+user_pref("ui.key.menuAccessKey", 1); // 9000
+user_pref("browser.fullscreen.autohide", false); // 9000
+user_pref("signon.rememberSignons", false); // 9000
+user_pref("browser.toolbars.bookmarks.visibility", always); // 9000
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // 9000
+user_pref("full-screen-api.enabled", false);
