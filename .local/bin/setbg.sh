64a516691617cb922971a5ec5697db4774075ab4
Miika Nissi <miika@miikanissi.com>
2022-06-07 17:08:52 -0400

    set background for all monitors

--- .local/bin/setbg.sh
+++ .local/bin/setbg.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-get_active_monitors()
-{
-    xrandr | awk '/\ connected/ && /[[:digit:]]x[[:digit:]].*+/{print $1}'
-}
-COUNT=$(get_active_monitors | wc -l)
-
 [ -f "$1" ] && cp "$1" ~/.local/share/wall.jpg
-[ -f "$2" ] && cp "$2" ~/.local/share/wall-vertical.jpg
 
-if [ "$COUNT" -eq 2 ]; then
-  feh --no-fehbg --bg-scale ~/.local/share/wall.jpg --bg-scale ~/.local/share/wall-vertical.jpg
-elif [ "$COUNT" -gt 2 ]; then
-  feh --no-fehbg --bg-scale ~/.local/share/wall.jpg --no-fehbg --bg-scale ~/.local/share/wall.jpg --no-fehbg --bg-scale ~/.local/share/wall-vertical.jpg
-else
-  feh --no-fehbg --bg-scale ~/.local/share/wall.jpg
-fi
+feh --no-fehbg --bg-scale ~/.local/share/wall.jpg
