10c9b1f48b787e8ed895cdd309219eec5384cdcb
Miika Nissi <miika@miikanissi.com>
2022-06-08 09:13:02 -0400

    update font script to get latest release

--- .local/bin/install_nerd_fonts_ubuntu_mono.sh
+++ .local/bin/install_nerd_fonts_ubuntu_mono.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # curls the Ubuntu/Ubuntu Mono Nerd Fonts v2.1.0 and installs it to ~/.local/share/fonts
 dl_dir=${XDG_DOWNLOAD_DIR:-$HOME/Downloads}
-curl -L https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/UbuntuMono.zip -o $dl_dir/UbuntuMono.zip
+curl -L https://github.com/ryanoasis/nerd-fonts/releases/download/2.2.0-RC/UbuntuMono.zip -o $dl_dir/UbuntuMono.zip
 unzip -u $dl_dir/UbuntuMono.zip -d ~/.local/share/fonts/
-curl -L https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Ubuntu.zip -o $dl_dir/Ubuntu.zip
+curl -L https://github.com/ryanoasis/nerd-fonts/releases/download/2.2.0-RC/Ubuntu.zip -o $dl_dir/Ubuntu.zip
 unzip -u $dl_dir/Ubuntu.zip -d ~/.local/share/fonts/
 fc-cache -fv
