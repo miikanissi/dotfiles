7548fcbc576e1cb0fbc774569adbbfba37d9fb23
Miika Nissi <miika@miikanissi.com>
2022-10-15 19:30:31 -0400

    Organized files and misc smb share changes

--- .local/bin/rsync_website.sh
+++ .local/bin/rsync_website.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -uvrP -e 'ssh -p 69' --delete-after ~/Documents/Projects/miikanissi.com/public/ root@miikanissi.com:/var/www/www/
+rsync -uvrP -e 'ssh -p 69' --delete-after ~/Documents/miikanissi.com/public/ root@miikanissi.com:/var/www/www/
