36cb9319b698790537e7deb2a3a143bc62574fe3
Miika Nissi <miika@miikanissi.com>
2022-10-27 21:57:32 -0400

    Updated config to nvim 0.8.0

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -317,7 +317,7 @@ end
 
 -- nvim-cmp supports additional completion capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Enable the following language servers. Order matters
 -- tsserver (TypeScript, JavaScript)
