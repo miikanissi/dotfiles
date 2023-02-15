fca484e091eb07a1dd67262ca96487108a79a417
Miika Nissi <miika@miikanissi.com>
2023-02-14 23:22:59 -0500

    which key plugin settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -191,11 +191,14 @@ require("lazy").setup({
 		"folke/which-key.nvim",
 		config = function()
 			vim.o.timeout = true
-			vim.o.timeoutlen = 300
+			vim.o.timeoutlen = 500
 			require("which-key").setup({
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
+				plugins = {
+					spelling = {
+						enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+						suggestions = 20, -- how many suggestions should be shown in the list?
+					},
+				},
 			})
 		end,
 	},
