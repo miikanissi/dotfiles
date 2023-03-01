3836ff391e5e1a0482280f16a4b089384d685e8b
Miika Nissi <miika@miikanissi.com>
2023-03-01 00:12:15 -0500

    Add autocompletion for odoo-docker
    Renamed odoo_docker as odoo-docker. Renamed odoo_logs as odoo-logs.

--- .local/bin/rsync_usb.sh
+++ .local/bin/rsync_usb.sh
@@ -2,15 +2,14 @@
 mount_point=/mnt/usb
 
 if mountpoint -q -- $mount_point; then
-  sudo rsync -avhPWr --delete-after \
-      /home/m/Documents \
-      /home/m/Mail \
-      /home/m/Videos \
-      /home/m/Pictures \
-      /home/m/Music \
-      /home/m/Pictures \
-      /home/m/.ssh \
-      /home/m/.gnupg \
-      /home/m/.password-store \
-      $mount_point/miika-backup
+	sudo rsync -avhPWr --delete-after \
+		/home/m/Documents \
+		/home/m/Mail \
+		/home/m/Videos \
+		/home/m/Pictures \
+		/home/m/Music \
+		/home/m/Pictures \
+		/home/m/.ssh \
+		/home/m/.gnupg \
+		$mount_point/miika-backup
 fi
