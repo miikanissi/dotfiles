b3862b6bb8c6f58588979fbb54f2256e9c706ad7
Miika Nissi <miika@miikanissi.com>
2023-04-23 20:15:19 -0400

    updated programs and sync script

--- .local/bin/rsync_usb.sh
+++ .local/bin/rsync_usb.sh
@@ -3,12 +3,12 @@ mount_point=/mnt/usb
 
 if mountpoint -q -- $mount_point; then
 	sudo rsync -avhPWr --delete-after \
+		/home/m/Applications \
 		/home/m/Documents \
 		/home/m/Mail \
-		/home/m/Videos \
-		/home/m/Pictures \
 		/home/m/Music \
 		/home/m/Pictures \
+		/home/m/Videos \
 		/home/m/.ssh \
 		/home/m/.gnupg \
 		$mount_point/miika-backup
